aedace4827ff85a6c94b59ca6b0fd31dc348fe59
Peter Bjorklund <piot@hotmail.com>
2022-08-20 09:46:57 +0200

    chore: add missing header

--- src/lib/Hazy/InternetSimulator.cs
+++ src/lib/Hazy/InternetSimulator.cs
@@ -1,3 +1,8 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
 using System;
 using Piot.Clog;
 using Piot.MonotonicTime;
