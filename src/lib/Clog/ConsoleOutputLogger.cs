1afb4273937abfbc1c2a85a0793a131412776cf6
Peter Bjorklund <piot@hotmail.com>
2022-09-05 13:08:38 +0200

    fix: write color chart

--- src/lib/Clog/ConsoleOutputLogger.cs
+++ src/lib/Clog/ConsoleOutputLogger.cs
@@ -10,35 +10,55 @@ namespace Piot.Clog
 {
     public class ConsoleOutputLogger : ILogTarget
     {
+        private const string resetColor = "\x1b[0m";
+
         public void Log(LogLevel level, string prefix, string message, object[] args)
         {
             var strings = args.Select(x => x.ToString());
             var values = args.Length > 0 ? $"({string.Join(", ", strings)})" : "";
             var color = ColorStringFromLogLevel(level);
-            const string resetColor = "\x1b[0m";
+
             var line = $"{color}{level,8}{resetColor} : [{prefix}] {message} {values}";
 
             Console.WriteLine(line);
         }
 
-        private static (int, int) ColorValuesFromLogLevel(LogLevel level)
+        private static (bool, int) ColorValuesFromLogLevel(LogLevel level)
         {
             return level switch
             {
-                LogLevel.LowLevel => (0, 34),
-                LogLevel.Debug => (1, 36),
-                LogLevel.Info => (1, 32),
-                LogLevel.Notice => (1, 35),
-                LogLevel.Warning => (1, 33),
-                LogLevel.Error => (0, 31),
+                LogLevel.LowLevel => (false, 4),
+                LogLevel.Debug => (true, 6),
+                LogLevel.Info => (true, 2),
+                LogLevel.Notice => (true, 5),
+                LogLevel.Warning => (true, 3),
+                LogLevel.Error => (false, 1),
                 _ => throw new Exception("illegal LogLevel")
             };
+        }
+
+        private static string ForegroundColorString(bool bright, int foregroundColor)
+        {
+            var prefix = bright ? "1" : "0";
+            return "\x1b" + $"[{prefix};{30 + foregroundColor}m";
+        }
+
+        public void WriteColorChart()
+        {
+            string[] colorStrings = { "black", "red", "green", "yellow", "blue", "magenta", "cyan", "white" };
+
+            for (var i = 0; i < 8; ++i)
+            {
+                var colorName = colorStrings[i];
+                Console.WriteLine($"{i} {ForegroundColorString(false, i)}{colorName}{resetColor}");
+                Console.WriteLine($"{i} {ForegroundColorString(true, i)}bright {colorName}{resetColor}");
+            }
         }
 
         private static string ColorStringFromLogLevel(LogLevel level)
         {
             var (prefix, foregroundColor) = ColorValuesFromLogLevel(level);
-            return "\x1b" + $"[{prefix};{foregroundColor}m";
+            return ForegroundColorString(prefix, foregroundColor);
         }
     }
 }
