b98decd69275489c985451dce467948d9abe97cf
Peter Bjorklund <piot@hotmail.com>
2022-09-09 14:30:40 +0200

    refactor: client datagram receiver

--- src/lib/Pulse/Client/Client.cs
+++ src/lib/Pulse/Client/Client.cs
@@ -4,18 +4,10 @@
  *--------------------------------------------------------------------------------------------*/
 
 using Piot.Clog;
-using Piot.Flood;
 using Piot.MonotonicTime;
-using Piot.Stats;
 using Piot.Surge.Compress;
-using Piot.Surge.DatagramType.Serialization;
 using Piot.Surge.Event;
 using Piot.Surge.LogicalInput;
-using Piot.Surge.MonotonicTimeLowerBits;
-using Piot.Surge.OrderedDatagrams;
-using Piot.Surge.SnapshotProtocol.Fragment;
-using Piot.Surge.SnapshotProtocol.In;
-using Piot.Surge.Tick.Serialization;
 using Piot.Transport;
 using Piot.Transport.Stats;
 
@@ -23,26 +15,19 @@ namespace Piot.Surge.Pulse.Client
 {
     public class Client
     {
-        private readonly IMultiCompressor compression;
+        private readonly ClientDatagramReceiver datagramReceiver;
         private readonly ClientDeltaSnapshotPlayback deltaSnapshotPlayback;
-        private readonly HoldPositive isReceivingMergedSnapshots = new(20);
         private readonly ILog log;
-        private readonly OrderedDatagramsInChecker orderedDatagramsInChecker = new();
         private readonly ClientPredictor predictor;
-        private readonly SnapshotFragmentReAssembler snapshotFragmentReAssembler;
-        private readonly StatCountThreshold statsHostInputQueueCount = new(60);
-        private readonly StatCountThreshold statsRoundTripTime = new(20);
         private readonly ITransportClient transportClient;
         private readonly TransportStatsBoth transportWithStats;
-        private readonly HoldPositive weAreSkippingAhead = new(25);
 
         public Client(ILog log, Milliseconds now, Milliseconds targetDeltaTimeMs,
             IEntityContainerWithGhostCreator worldWithGhostCreator, IEventProcessorWithCreate eventProcessorWithCreate,
             ITransport assignedTransport, IMultiCompressor compression, IInputPackFetch fetch)
         {
             this.log = log;
-            this.compression = compression;
-            snapshotFragmentReAssembler = new(log);
+
             World = worldWithGhostCreator;
             transportWithStats = new(assignedTransport, now);
             transportClient = new TransportClient(transportWithStats);
@@ -53,6 +38,8 @@ namespace Piot.Surge.Pulse.Client
                 new ClientDeltaSnapshotPlayback(now, worldWithGhostCreator, eventProcessorWithCreate, predictor,
                     targetDeltaTimeMs,
                     log.SubLog("GhostPlayback"));
+
+            datagramReceiver = new(transportClient, compression, deltaSnapshotPlayback, predictor, log);
         }
 
         public IInputPackFetch InputFetch
@@ -62,119 +49,15 @@ namespace Piot.Surge.Pulse.Client
 
         public IEntityContainerWithGhostCreator World { get; }
 
-        public ClientNetworkQuality NetworkQuality =>
-            new()
-            {
-                isSkippingSnapshots = weAreSkippingAhead.IsOrWasTrue,
-                isReceivingMergedSnapshots = isReceivingMergedSnapshots.IsOrWasTrue,
-                isIncomingSnapshotPlaybackBufferStarving = deltaSnapshotPlayback.IsIncomingBufferStarving,
-                averageRoundTripTimeMs = (uint)statsRoundTripTime.Stat.average
-            };
-
-        private void ReceiveSnapshotExtraData(IOctetReader reader, Milliseconds now)
-        {
-            var pongTimeLowerBits = MonotonicTimeLowerBitsReader.Read(reader);
-            var pongTime = LowerBitsToMonotonic.LowerBitsToMonotonicMs(now, pongTimeLowerBits);
-            var roundTripTimeMs = now.ms - pongTime.ms;
-            statsRoundTripTime.Add((int)roundTripTimeMs);
-            log.DebugLowLevel("RoundTripTime {RoundTripTimeMs} {AverageRoundTripTimeMs}", roundTripTimeMs,
-                statsRoundTripTime.Stat.average);
-
-            var numberOfInputInQueue = reader.ReadInt8();
-            statsHostInputQueueCount.Add(numberOfInputInQueue);
-
-            var serverIsProcessingTickId = TickIdReader.Read(reader);
-
-            if (statsRoundTripTime.IsReady)
-            {
-                predictor.AdjustPredictionSpeed(serverIsProcessingTickId, (uint)statsRoundTripTime.Stat.average);
-            }
-
-            log.DebugLowLevel("InputQueueCountFromHost {InputQueueCount} {AverageInputQueueCount}",
-                numberOfInputInQueue, statsHostInputQueueCount.Stat.average);
-        }
-
-        private void ReceiveSnapshot(IOctetReader reader, Milliseconds now)
-        {
-            log.DebugLowLevel("receiving snapshot datagram from server");
-            ReceiveSnapshotExtraData(reader, now);
-            var snapshotIsDone = snapshotFragmentReAssembler.Read(reader, out var tickIdRange, out var completePayload);
-            predictor.LastSeenSnapshotTickId = tickIdRange.Last;
-
-            if (!snapshotIsDone)
-            {
-                return;
-            }
-
-            if (!deltaSnapshotPlayback.WantsSnapshotWithTickIdRange(tickIdRange))
-            {
-                return;
-            }
-
-            var snapshotWithCorrections =
-                DeltaSnapshotIncludingCorrectionsReader.Read(tickIdRange, completePayload, compression);
-
-            deltaSnapshotPlayback.FeedSnapshotDeltaPack(snapshotWithCorrections);
-
-            weAreSkippingAhead.Value = deltaSnapshotPlayback.LastPlaybackSnapshotWasSkipAhead;
-            if (weAreSkippingAhead.IsOrWasTrue)
-            {
-                log.Notice("we are or have been skipping ahead!");
-            }
-
-            isReceivingMergedSnapshots.Value = deltaSnapshotPlayback.LastPlaybackSnapshotWasMerged;
-            if (isReceivingMergedSnapshots.Value)
-            {
-                log.Notice("we are receiving merged snapshots");
-            }
-
-            predictor.NextExpectedSnapshotTickId = snapshotWithCorrections.tickIdRange.lastTickId.Next();
-        }
-
-        private void ReceiveDatagramFromHost(IOctetReader reader, Milliseconds now)
-        {
-            if (!orderedDatagramsInChecker.ReadAndCheck(reader))
-            {
-                log.Notice("ordered datagram in wrong order, discarding datagram {OrderedDatagramsSequenceId}",
-                    orderedDatagramsInChecker);
-                return;
-            }
-
-            var datagramType = DatagramTypeReader.Read(reader);
-            switch (datagramType)
-            {
-                case DatagramType.DatagramType.DeltaSnapshots:
-                    ReceiveSnapshot(reader, now);
-                    break;
-                default:
-                    throw new DeserializeException($"illegal datagram type {datagramType} from host");
-            }
-        }
-
-        private void ReceiveDatagramsFromHost(Milliseconds now)
-        {
-            for (var i = 0; i < 30; i++)
-            {
-                var datagram = transportClient.ReceiveFromHost();
-                if (datagram.IsEmpty)
-                {
-                    return;
-                }
-
-                var datagramReader = new OctetReader(datagram);
-                ReceiveDatagramFromHost(datagramReader, now);
-            }
-        }
-
         public void Update(Milliseconds now)
         {
-            ReceiveDatagramsFromHost(now);
+            datagramReceiver.ReceiveDatagramsFromHost(now);
             transportWithStats.Update(now);
             predictor.Update(now);
             deltaSnapshotPlayback.Update(now);
             var readStats = transportWithStats.Stats;
             log.DebugLowLevel("stats: {Stats}", readStats);
-            log.DebugLowLevel("netStats: {Stats}", NetworkQuality);
+            log.DebugLowLevel("netStats: {Stats}", datagramReceiver.NetworkQuality);
         }
     }
 }
---
+++ src/lib/Pulse/Client/ClientDatagramReceiver.cs
@@ -0,0 +1,150 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Peter Bjorklund. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+using Piot.Clog;
+using Piot.Flood;
+using Piot.MonotonicTime;
+using Piot.Stats;
+using Piot.Surge.Compress;
+using Piot.Surge.DatagramType.Serialization;
+using Piot.Surge.MonotonicTimeLowerBits;
+using Piot.Surge.OrderedDatagrams;
+using Piot.Surge.SnapshotProtocol.Fragment;
+using Piot.Surge.SnapshotProtocol.In;
+using Piot.Surge.Tick.Serialization;
+using Piot.Transport;
+
+namespace Piot.Surge.Pulse.Client
+{
+    public class ClientDatagramReceiver
+    {
+        private readonly IMultiCompressor compression;
+        private readonly HoldPositive isReceivingMergedSnapshots = new(20);
+        private readonly ClientDeltaSnapshotPlayback notifyPlayback;
+        private readonly ClientPredictor notifyPredictor;
+        private readonly OrderedDatagramsInChecker orderedDatagramsInChecker = new();
+        private readonly SnapshotFragmentReAssembler snapshotFragmentReAssembler;
+        private readonly StatCountThreshold statsHostInputQueueCount = new(60);
+        private readonly StatCountThreshold statsRoundTripTime = new(20);
+        private readonly ITransportClient transportClient;
+        private readonly HoldPositive weAreSkippingAhead = new(25);
+        private readonly ILog log;
+
+        public ClientDatagramReceiver(ITransportClient transportClient, IMultiCompressor compression,
+            ClientDeltaSnapshotPlayback notifyPlayback, ClientPredictor notifyPredictor, ILog log)
+        {
+            this.log = log;
+            this.compression = compression;
+            this.notifyPlayback = notifyPlayback;
+            this.notifyPredictor = notifyPredictor;
+            this.transportClient = transportClient;
+            snapshotFragmentReAssembler = new(log);
+        }
+
+        public ClientNetworkQuality NetworkQuality =>
+            new()
+            {
+                isSkippingSnapshots = weAreSkippingAhead.IsOrWasTrue,
+                isReceivingMergedSnapshots = isReceivingMergedSnapshots.IsOrWasTrue,
+                isIncomingSnapshotPlaybackBufferStarving = notifyPlayback.IsIncomingBufferStarving,
+                averageRoundTripTimeMs = (uint)statsRoundTripTime.Stat.average
+            };
+
+        private void ReceiveSnapshotExtraData(IOctetReader reader, Milliseconds now)
+        {
+            var pongTimeLowerBits = MonotonicTimeLowerBitsReader.Read(reader);
+            var pongTime = LowerBitsToMonotonic.LowerBitsToMonotonicMs(now, pongTimeLowerBits);
+            var roundTripTimeMs = now.ms - pongTime.ms;
+            statsRoundTripTime.Add((int)roundTripTimeMs);
+            log.DebugLowLevel("RoundTripTime {RoundTripTimeMs} {AverageRoundTripTimeMs}", roundTripTimeMs,
+                statsRoundTripTime.Stat.average);
+
+            var numberOfInputInQueue = reader.ReadInt8();
+            statsHostInputQueueCount.Add(numberOfInputInQueue);
+
+            var serverIsProcessingTickId = TickIdReader.Read(reader);
+
+            if (statsRoundTripTime.IsReady)
+            {
+                notifyPredictor.AdjustPredictionSpeed(serverIsProcessingTickId, (uint)statsRoundTripTime.Stat.average);
+            }
+
+            log.DebugLowLevel("InputQueueCountFromHost {InputQueueCount} {AverageInputQueueCount}",
+                numberOfInputInQueue, statsHostInputQueueCount.Stat.average);
+        }
+
+        private void ReceiveSnapshot(IOctetReader reader, Milliseconds now)
+        {
+            log.DebugLowLevel("receiving snapshot datagram from server");
+            ReceiveSnapshotExtraData(reader, now);
+            var snapshotIsDone = snapshotFragmentReAssembler.Read(reader, out var tickIdRange, out var completePayload);
+            notifyPredictor.LastSeenSnapshotTickId = tickIdRange.Last;
+
+            if (!snapshotIsDone)
+            {
+                return;
+            }
+
+            if (!notifyPlayback.WantsSnapshotWithTickIdRange(tickIdRange))
+            {
+                return;
+            }
+
+            var snapshotWithCorrections =
+                DeltaSnapshotIncludingCorrectionsReader.Read(tickIdRange, completePayload, compression);
+
+            notifyPlayback.FeedSnapshotDeltaPack(snapshotWithCorrections);
+
+            weAreSkippingAhead.Value = notifyPlayback.LastPlaybackSnapshotWasSkipAhead;
+            if (weAreSkippingAhead.IsOrWasTrue)
+            {
+                log.Notice("we are or have been skipping ahead!");
+            }
+
+            isReceivingMergedSnapshots.Value = notifyPlayback.LastPlaybackSnapshotWasMerged;
+            if (isReceivingMergedSnapshots.Value)
+            {
+                log.Notice("we are receiving merged snapshots");
+            }
+
+            notifyPredictor.NextExpectedSnapshotTickId = snapshotWithCorrections.tickIdRange.lastTickId.Next();
+        }
+
+        private void ReceiveDatagramFromHost(IOctetReader reader, Milliseconds now)
+        {
+            if (!orderedDatagramsInChecker.ReadAndCheck(reader))
+            {
+                log.Notice("ordered datagram in wrong order, discarding datagram {OrderedDatagramsSequenceId}",
+                    orderedDatagramsInChecker);
+                return;
+            }
+
+            var datagramType = DatagramTypeReader.Read(reader);
+            switch (datagramType)
+            {
+                case DatagramType.DatagramType.DeltaSnapshots:
+                    ReceiveSnapshot(reader, now);
+                    break;
+                default:
+                    throw new DeserializeException($"illegal datagram type {datagramType} from host");
+            }
+        }
+
+        public void ReceiveDatagramsFromHost(Milliseconds now)
+        {
+            for (var i = 0; i < 30; i++)
+            {
+                var datagram = transportClient.ReceiveFromHost();
+                if (datagram.IsEmpty)
+                {
+                    return;
+                }
+
+                var datagramReader = new OctetReader(datagram);
+                ReceiveDatagramFromHost(datagramReader, now);
+            }
+        }
+    }
+}
