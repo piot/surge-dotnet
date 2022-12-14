63d4e7038429e1ba5696263bb692bd72113dd18d
Peter Bjorklund <piot@hotmail.com>
2022-12-14 22:28:27 +0100

    chore: remove dependencies

--- src/lib/Runtime/Base/Clog/ConsoleOutputLogger.cs
+++ src/lib/Runtime/Base/Clog/ConsoleOutputLogger.cs
@@ -4,10 +4,7 @@
  *--------------------------------------------------------------------------------------------*/
 
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
-using UnityEngine;
 
 namespace Piot.Clog
 {
